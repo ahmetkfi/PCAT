86232af18d3f65d7110bd9aeb0649bf66e5be7b4
ahmetkfi <ahmetkcygt58@gmail.com>
2023-04-21 20:46:37 +0300

    "second commit"

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    const photo={
+        id:1,
+        name : 'photo-name',
+        desc:'photo-description'
+    };
+    res.send(photo);
+});
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`Sunucu port ${port} den baslatildi`);
+});
