5b72d3557be093ca6e002636550efc8a5deb1ea5
ahmetkfi <ahmetkcygt58@gmail.com>
2023-04-23 22:02:02 +0300

    23042023 commit

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+const path = require('path');
+const exp = require('constants');
+const mongoose=require('mongoose');
+//photo modelini export ile aldık
+const Photo=require('./models/Photo');
 
-app.get('/',(req,res)=>{
-    const photo={
-        id:1,
-        name : 'photo-name',
-        desc:'photo-description'
-    };
-    res.send(photo);
+//DB CONNECTION
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));//post ile veri çekerken urldeki datayı okumak için
+app.use(express.json());//post ile veri çekerken urldeki datayı json formatına dönüştürmek için
+
+//ROUT
+app.get('/', async(req, res) => {
+  const photos=await Photo.find({});
+  res.render('index',{
+    photos
+  });
+});
+app.get('/about', (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('add');
+});
+//POST İŞLEMLERİ
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
-const port=3000;
-app.listen(port,()=>{
-    console.log(`Sunucu port ${port} den baslatildi`);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} den baslatildi`);
 });
